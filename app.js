593b7217d84b1508f3f43d19c9cf6774b8d6584f
Collins Kariuki <collinsmunkary@gmail.com>
2022-10-26 09:42:38 +0300

    Middleware and letting requests move from one middleware to the next

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const app = express(); //initializes a new object where expressjs
 //will store and manage a lot of things for us behind the scenes
 //a lot of logic is in the app constant
 
+//using middleware
+//use allows us to add a new middleware function
+app.use((req,res,next) => {
+    console.log("In the middleware!");
+    next(); //Allows the request to continue to the next middleware in line
+    //use next to allow a request to move onto the next function
+    //you should send a response if you've got other plans
+}); 
+
+app.use((req,res,next) => {
+    console.log("In another middleware!");
+});
+
 const server = http.createServer(app); //app is a valid request handler
 
 
