08b86175edd1f39b709a20dd264aa08dd1d5d578
Collins Kariuki <collinsmunkary@gmail.com>
2022-11-29 14:42:52 +0300

    Add controllers first steps

--- routes/shop.js
+++ routes/shop.js
@@ -2,16 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products')
+//the two lines below were removed after adding controllers in products.js
+// const rootDir = require('../util/path');
+// const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    //take products from admin data
-    const products = adminData.products;
-    //inject that data you took into our template
-    res.render('shop', {prods: products, pageTitle: 'Shop', path:'/'});
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
